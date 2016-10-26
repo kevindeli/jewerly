ffb1af21ae218e96414a5546e2aa4172e7488a24
Kevin Deli <kevin@ssd.co.il>
2016-10-26 16:07:01 +0300

    cms style app.scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function(){
-    gulp.src('assats/sass/*')
+    gulp.src('assats/scss/*')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('public/css'));
+    gulp.src('assats/scss/fa/*')
+        .pipe(gulp.dest('public/css/fa'));
 });
